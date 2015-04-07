3d7f25f24daae1ec263870acc03032dbaad6df99
John Hogarty <hogihung@gmail.com>
2015-04-06 20:52:01 -0400

    Toggle switch and locked text input working as designed.

--- NumberAndTextLockDown/ViewController.swift
+++ NumberAndTextLockDown/ViewController.swift
@@ -17,14 +17,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Disable the lockedText text on load
+        lockedText.enabled = false
+        
+        // Let's turn the switch off on load
+        unlockText.on = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // zipCode text box should only allows numbers with a 
+    // max number of 5 total numbers.
+    
+    // showMoney should allow the user to type dollar value
+    // then cents
 
-
+    
+    @IBAction func toggleUnlockText(sender: UISwitch) {
+        println("SwitchState: \(sender.on)")
+        
+        if unlockText.on {
+            lockedText.enabled = true
+        } else {
+            lockedText.enabled = false
+        }
+    }
 }
 
